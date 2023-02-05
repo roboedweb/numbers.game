35ed22806150016f75b3a83318caf5cf21e976f3
roboedweb <dved@roboua.org>
2023-02-05 14:38:15 +0200

    update

--- numbers.js
+++ numbers.js
@@ -3,8 +3,12 @@ const maxWindow = document.querySelector('.max');
 const minus = document.querySelector('.minus');
 const plus = document.querySelector('.plus');
 const diapozonAlert = document.querySelector('.diapozon');
-let diapozon, tries, randomNumber;
+const numberField = document.getElementById('search-number');
+const infoField = document.querySelector('.info-wrapper');
+let diapozon, tries, randomNumber, number;
 maxWindow.innerHTML = slider.value;
+
+numberField.disabled = true;
 diapAlert();
 
 slider.oninput = function() {
@@ -33,27 +37,34 @@ function diapAlert() {
     diapozonAlert.innerHTML = `Потрібно вгадати число в діапазоні від 1 до ${slider.value}`;
 }
 
-function numbersGame() {
-    alert('OK');
-    
-    // let number;
-
-    // for(let i = 1; i <= tries; i++){
-    //     number = +prompt('Введіть ваш варінт');
-    //         if(number < randomNumber) {
-    //             alert('Загадане число більше за введене');
-    //             alert(`У вас залишилось ${tries - i} спроб`);
-    //         }
-    //         else if(number === randomNumber){
-    //             alert('Ви виграли! Супер!');
-    //             alert(`Ми вгадали за ${i} спроб `);
-    //             break;
-    //         }
-    //         else{
-    //             alert('Загадане число менше за введене');
-    //             alert(`У вас залишилось ${tries - i} спроб`);
-    //         }
-    //     }
+function numbersGame(i) {
+    document.querySelector(".number-icon").onclick = function() {
+        number = numberField.value;
+        numberField.value = '';
+        if (i <= tries && i > 0) {
+            if (isNaN(Number(number)) || Number(number) == 0) {
+                infoField.innerHTML = 'Введіть, будь ласка, коректне число!';
+                i--;
+            }
+            else if(Number(number) < randomNumber) {
+                infoField.innerHTML = 'Загадане число більше за введене<br>' + `У вас залишилось ${tries - i} спроб`;
+            }
+            else if(Number(number) == randomNumber){
+                infoField.innerHTML = 'Ви виграли! Супер!<br>' + `Ви вгадали за ${i} спроб `;
+                numberField.disabled = true;
+                start.disabled = false;
+            }
+            else{
+                infoField.innerHTML = 'Загадане число менше за введене<br>' + `У вас залишилось ${tries - i} спроб`;
+            }
+        }
+        else {
+            infoField.innerHTML = 'Ви, на жаль, програли';
+            numberField.disabled = true;
+            start.disabled = false;
+        }
+        i++;
+    }
 }
 
 function generateNum() {
@@ -76,5 +87,14 @@ function generateNum() {
         }
     }
     randomNumber = Math.floor(Math.random() * diapozon + 1);
-    numbersGame();
+    numberField.disabled = false;
+    start.disabled = true;
+    numbersGame(1);
 }
+
+document.getElementById('search-number').addEventListener('keypress', function(event) {
+    if (event.key == 'Enter') {
+        event.preventDefault();
+        document.querySelector('.number-icon').click();
+    }
+});
